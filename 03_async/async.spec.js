73bbecf519bd71c36fc26b7d537e55b48f2a4e58
Maxim Klochkov <m.klochkov@sap.com>
2021-10-01 18:31:57 +0300

    added axios

--- 03_async/async.spec.js
+++ 03_async/async.spec.js
@@ -1,4 +1,7 @@
+const axios = require('axios')
 const Ajax = require('./async')
+
+jest.mock('axios')
 
 describe('Ajax: echo', () => {
     test('Should return value Async', async () => {
@@ -24,6 +27,31 @@ describe('Ajax: echo', () => {
     test('Should catch error with Promise', () => {
         return Ajax.echo().catch(err => {
             expect(err).toBeInstanceOf(Error)
+        })
+    })
+})
+
+describe('Ajax: GET', () => {
+    let todos
+    let response
+
+    beforeEach(() => {
+        todos = [
+            { id: 1, title: 'Todo 1', completed: false  }
+        ]
+
+        response = {
+            data: {
+                todos
+            }
+        }
+    })
+
+    test('Should return data from backend', () => {
+        axios.get.mockReturnValue(response)
+
+        return Ajax.get().then(data => {
+            expect(data.todos).toEqual(todos)
         })
     })
 })
